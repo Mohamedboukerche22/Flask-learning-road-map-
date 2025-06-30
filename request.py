aaa1baf6cf19c4f1732ad878179ab558a0a52029
Mohamedboukerche22 <mohamedboukerche55@gmail.com>
2025-07-01 00:10:47 +0100

    Create request.py

---
+++ request.py
@@ -0,0 +1,18 @@
+from flask import Flask,request
+
+app = Flask(__name__)
+
+@app.route('/greet', methods = ['GET' , 'POST'])
+def greet():
+    if request.method == 'POST':
+        name = request.form['name']
+        return f"hello {name}"
+    return '''
+    <form method = 'POST'>
+    <input name = "name">
+    <input type ="submit">
+    </form>
+    '''
+
+if __name__ =='__main__':
+    app.run(debug = True)
