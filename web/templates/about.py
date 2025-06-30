c82d16eddb9ac73db82ad71e5adbbc8f14af6ef7
Mohamedboukerche22 <mohamedboukerche55@gmail.com>
2025-07-01 00:23:36 +0100

    Create about.py

---
+++ web/templates/about.py
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>حول الموقع</title>
+</head>
+<body>
+    <h1>من نحن؟</h1>
+    <p>نحن فريق يستخدم Flask لصناعة المواقع.</p>
+    <a href="/">العودة إلى الرئيسية</a>
+</body>
+</html>
